fd80c553c15032c1f249238f41be3f5f26838f8c
Daniel Luxemburg <daniel.luxemburg@gmail.com>
2013-09-21 15:09:11 -0400

    don't use async in test script

--- lib/loggr-rb/integration/tester.rb
+++ lib/loggr-rb/integration/tester.rb
@@ -4,7 +4,7 @@ module Loggr
     end
 
     def self.test
-	  Loggr::Events.create().text("Test event").tags("test").source("loggr-rb-#{Loggr::VERSION}").post()
+	  Loggr::Events.create().text("Test event").tags("test").source("loggr-rb-#{Loggr::VERSION}").post(false)
 	  puts "Test Event sent. Please login to http://loggr.net to see it!"
     end
   end
